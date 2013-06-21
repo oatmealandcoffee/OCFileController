3730c9ece28200c87ba63349681536fa3c817282
Philip Regan <oatmealandcoffee@gmail.com>
2013-06-21 10:41:53 -0700

    Updating copyright and licensing

--- OCFileController.m
+++ OCFileController.m
@@ -27,16 +27,16 @@
     if (message) {
         [panel setMessage:message];
     }
-    
+
     NSInteger result = [panel runModal];
     NSURL *folder;
-    
+
     if ( result == NSFileHandlingPanelOKButton ) {
         folder = [[panel URLs] objectAtIndex:0];
     }
-    
+
     return folder;
-    
+
 }
 
 - (NSURL *) selectFile:(NSString *)message {
@@ -49,131 +49,131 @@
     [panel setAllowsMultipleSelection:NO];
     if (message) {
         [panel setMessage:message];
-    } 
-    
+    }
+
     NSInteger result = [panel runModal];
     NSURL *file;
-    
+
     if ( result == NSFileHandlingPanelOKButton ) {
         file = [[panel URLs] objectAtIndex:0];
     }
-    
+
     return file;
-    
+
 }
 
 - (NSArray *) getFilesInFolder:(NSURL *)folder {
-    
+
     if (!folder) {
         return nil;
     }
-    
+
     NSFileManager *fm = [NSFileManager defaultManager];
-    
+
     NSError *readError;
-    NSArray *fileUrls = [fm contentsOfDirectoryAtURL:folder 
-                          includingPropertiesForKeys:[NSArray array] 
+    NSArray *fileUrls = [fm contentsOfDirectoryAtURL:folder
+                          includingPropertiesForKeys:[NSArray array]
                                              options:(NSDirectoryEnumerationSkipsSubdirectoryDescendants | NSDirectoryEnumerationSkipsPackageDescendants | NSDirectoryEnumerationSkipsHiddenFiles)
                                                error:&readError];
-    
+
     NSMutableArray *files = [NSMutableArray array];
-    
+
     for ( NSURL *url in fileUrls ) {
         [files addObject:[url path]];
     }
-    
+
     return [NSArray arrayWithArray:files];
 }
 
 #pragma mark - File I/O Stack -
 
 - (NSString *) stringFromFileInBundleWithName:(NSString *)fileName extension:(NSString *)fileExtension {
-    
+
     NSURL *dataURL = [[NSBundle mainBundle] URLForResource:fileName withExtension:fileExtension];
-    
+
     if (!dataURL) {
         return nil;
     }
-    
+
 	NSError *readError;
 	NSString *dataString = [NSString stringWithContentsOfURL:dataURL encoding:NSUTF8StringEncoding error:&readError];
 	if ( !dataString ) {
 		return nil;
 	}
-    
+
 	return dataString;
 }
 
 - (NSURL *) urlToFileInBundleWithName:(NSString *)fileName extension:(NSString *)fileExtension {
     return [[NSBundle mainBundle] URLForResource:fileName withExtension:fileExtension];
-    
+
 }
 
 - (NSError *) writeString:(NSString *)content toURL:(NSURL *)url withName:(NSString *)name {
-    
+
     if ( !content || !url || !name ) {
         return nil;
     }
-    
+
     NSURL *fileWritePath = [url URLByAppendingPathComponent:name isDirectory:NO];
-    
+
     if ( !fileWritePath ) {
         return nil;
     }
-    
+
     NSError *fileWriteError;
     [content writeToURL:fileWritePath atomically:NO encoding:NSUTF8StringEncoding error:&fileWriteError];
     return fileWriteError;
 }
 
 - (NSURL *) makeFolder:(NSString *)folderName parentFolder:(NSURL *)parent {
-    
+
     if ( !folderName || !parent ) {
         return nil;
     }
-    
+
 	NSURL *targetFolderURL = [parent URLByAppendingPathComponent:folderName];
 	NSString *targetFolderPath = [targetFolderURL path];
-    
+
 	NSFileManager *fileManager = [NSFileManager defaultManager];
-    
+
 	BOOL isDirectory;
-    
+
 	if ( [fileManager fileExistsAtPath:targetFolderPath isDirectory:&isDirectory] ) {
-        
+
 		NSError *remError;
 		BOOL folderRemoved = [fileManager removeItemAtPath:targetFolderPath error:&remError];
-        
+
 		if ( !folderRemoved ) {
 			return nil;
 		}
 	}
-    
+
 	NSError *dirError;
 	BOOL folderCreated = [fileManager createDirectoryAtPath:targetFolderPath withIntermediateDirectories:NO attributes:nil error:&dirError];
-    
+
 	if ( !folderCreated ) {
 		return nil;
 	}
-    
+
 	return targetFolderURL;
-    
+
 }
 
 - (BOOL) populateFolder:(NSURL *)folder withInternalAsset:(NSString *)fileName extension:(NSString *)fileExtension {
-    
+
     if ( !folder || !fileName || !fileExtension ) {
         return NO;
     }
-    
-    NSURL *assetUrl = [self urlToFileInBundleWithName:fileName 
+
+    NSURL *assetUrl = [self urlToFileInBundleWithName:fileName
                                             extension:fileExtension];
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSData *assetData = [fileManager contentsAtPath:[assetUrl path]];
-    NSURL *writeAssetUrl = [folder URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", fileName, fileExtension] 
+    NSURL *writeAssetUrl = [folder URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", fileName, fileExtension]
                                                    isDirectory:NO];
-    return [fileManager createFileAtPath:[writeAssetUrl path] 
-                                contents:assetData 
+    return [fileManager createFileAtPath:[writeAssetUrl path]
+                                contents:assetData
                               attributes:nil];
 }
 
@@ -193,17 +193,17 @@
 #pragma mark - Path Management Stack -
 
 - (NSString *) convertPosixPathtoHfsPath:(NSString *)posixPath isDirectory:(BOOL)isDirectory {
-    
+
     NSString *firstChar = [posixPath substringToIndex:1];
     if ( ![firstChar isEqualToString:@"/"] ) {
         return posixPath;
     }
-    
+
     CFURLRef myURL = CFURLCreateWithFileSystemPath(NULL, (__bridge CFStringRef)posixPath, kCFURLPOSIXPathStyle, isDirectory);
     NSString *hfsPath = (__bridge NSString *)CFURLCopyFileSystemPath(myURL, kCFURLHFSPathStyle);
     CFRelease(myURL);
     return hfsPath;
-    
+
 }
 
 @end
--- README.md
+++ README.md
@@ -14,3 +14,20 @@ This class takes a number of common file operations requiring lots of code and
  About error handling:
  * Nil will always be returned on any error unless otherwise noted.
  * Functions assume values being passed are valid. Unless a passed value is nil, Xcode will take over during debugging.
+
+ License
+ =======
+
+ Copyright 2013 Philip Regan, http://www.oatmealandcoffee.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
